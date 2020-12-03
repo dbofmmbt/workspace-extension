c14e1834b5a0b5dbbd958c21121bf3d1b2502ab9
Eduardo Canellas <eduardocanellas98@gmail.com>
2020-12-03 11:28:34 -0300

    feat(storage): implement Storage

--- src/Storage/index.ts
+++ src/Storage/index.ts
@@ -2,5 +2,24 @@ import { WorkspaceManager } from "../WorkspaceManager";
 
 export default interface Storage {
     save(ws: WorkspaceManager): void;
-    load(): WorkspaceManager;
+    load(): WorkspaceManager | null;
+}
+
+export class StorageImpl implements Storage {
+    storage_key: string = "workspace-extension";
+    _workspace_manager: WorkspaceManager | null = null;
+
+    save(ws: WorkspaceManager): void {
+        chrome.storage.sync.set({ [this.storage_key]: ws }, () => {
+            console.log("Workspace information stored.");
+        })
+    }
+
+    load(): WorkspaceManager | null {
+        chrome.storage.sync.get(this.storage_key, result => {
+            this._workspace_manager = result[this.storage_key];
+        })
+
+        return this._workspace_manager;
+    }
 }
