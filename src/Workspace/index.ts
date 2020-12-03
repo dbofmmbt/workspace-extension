3d50def23a4b7e45ad70a916c770dfc27fc3823d
Eduardo Canellas <eduardocanellas98@gmail.com>
2020-12-03 09:20:08 -0300

    feat(workspace): initial implementation for 'Workspace'

--- src/Workspace/index.ts
+++ src/Workspace/index.ts
@@ -10,3 +10,35 @@ export interface Workspace extends Open, Close {
     // Windows belonging to Workspace
     windows: Iterable<Window>;
 }
+
+export class WorkspaceImpl implements Workspace {
+    name: string;
+    windows: Array<Window> = [];
+    symbol: string;
+
+    constructor(name: string) {
+        this.name = name;
+        this.symbol = this.makeSymbol(name);
+    }
+
+    open(): void {
+        for (const window of this.windows) {
+            window.open();
+        }
+    }
+
+    close(): void {
+        for (const window of this.windows) {
+            window.close();
+        }
+    }
+
+    private makeSymbol(name: string): string {
+        let letters = this.name.match(/\b[a-zA-Z]/g);
+        if (letters !== null) {
+            return letters.join('');
+        } else {
+            return "";
+        }
+    }
+}
