f2caac1384e209c3b58a8138f0f55e39937a77b1
Eduardo Canellas <eduardocanellas98@gmail.com>
2020-12-02 22:51:48 -0300

    feat(tab): interface implemented

--- src/Workspace/Window/Tab/index.ts
+++ src/Workspace/Window/Tab/index.ts
@@ -5,3 +5,22 @@ export interface Tab extends Open, Close {
     url: string;
 }
 
+export class TabImpl implements Tab {
+    url: string;
+    id: number | undefined;
+
+    constructor(url: string) {
+        this.url = url;
+    }
+
+    open(): void {
+        chrome.tabs.create({ url: this.url }, tab => { this.id = tab.id });
+    }
+
+    close(): void {
+        if (this.id === undefined) {
+            return;
+        }
+        chrome.tabs.remove(this.id, () => this.id = undefined);
+    }
+}
