479c50e4cbbfdc6c3196945133b68b5ceeb760c1
Eduardo Canellas <eduardocanellas98@gmail.com>
2020-12-02 22:38:20 -0300

    feat(workspacemanager): interface defined

--- src/WorkspaceManager/index.ts
+++ src/WorkspaceManager/index.ts
@@ -1,2 +1,8 @@
-// TODO start interface definition
-export interface WorkspaceManager {}
+import { Workspace } from "../Workspace";
+
+export interface WorkspaceManager {
+    turn_active(workspace: Workspace): void;
+    save_state(): void;
+    active(): Workspace;
+    workspaces(): Iterable<Workspace>;
+}
