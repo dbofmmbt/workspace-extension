6ee680b869bfc9f0c0085c14cb0ba76b4fb609c9
Eduardo Canellas <eduardocanellas98@gmail.com>
2020-12-09 13:57:52 -0300

    feat(events): ´onInstalled` event implemented

--- src/events.ts
+++ src/events.ts
@@ -1,5 +1,12 @@
+import { fetchManager } from "./WorkspaceManager/setup";
+
 chrome.runtime.onInstalled.addListener(details => {
-    console.log(details);
+    console.debug("runtime.onInstalled:", details);
+
+    fetchManager().then(_manager => {
+        // Nothing to do. `fetchManager` will automatically
+        // set the initial Workspace and saved it locally.
+    });
 });
 
 chrome.runtime.onStartup.addListener(() => {
