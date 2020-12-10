dd90637df53b6af8f5e3365b2a725b46ff23ef2c
Eduardo Canellas <eduardocanellas98@gmail.com>
2020-12-10 11:51:05 -0300

    docs(startup_event): add comment about implementation

--- src/events.ts
+++ src/events.ts
@@ -10,7 +10,8 @@ chrome.runtime.onInstalled.addListener(details => {
 });
 
 chrome.runtime.onStartup.addListener(() => {
-    console.debug("runtime.OnStartup");
+    // In principle, there's nothing to do in this event, actually.
+    // Let's see if it remains that way in the test phase.
 });
 
 chrome.tabs.onCreated.addListener(tab => {
