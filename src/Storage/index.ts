44ab313ad400acafd453ae2a8c33e05d788e3278
Eduardo Canellas <eduardocanellas98@gmail.com>
2020-12-10 13:42:48 -0300

    docs(storage): add TODO to rewrite some methods

--- src/Storage/index.ts
+++ src/Storage/index.ts
@@ -8,6 +8,11 @@ export default interface Storage {
 export class StorageImpl implements Storage {
     storage_key: string = "workspace-extension";
 
+    // TODO Rewrite `save` and `load`.
+    // We can't save the Manager object in the storage because the object
+    // loses its methods. It'll be simpler to just separate the needed
+    // information on `save` and recreate the objects with that data
+    // later, on `load`.
     save(ws: WorkspaceManager): void {
         chrome.storage.sync.set({ [this.storage_key]: ws }, () => {
             console.log("Workspace information stored: ", ws);
