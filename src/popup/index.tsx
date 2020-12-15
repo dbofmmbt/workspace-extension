fdfbd3a214ef6559f4182272a70c1cbb7fc6b4bb
Eduardo Canellas <eduardocanellas98@gmail.com>
2020-12-15 14:00:35 -0300

    style(popup_index): add underline to an unused variable

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Popup from './Popup';
 
-chrome.tabs.query({ active: true, currentWindow: true }, tab => {
+chrome.tabs.query({ active: true, currentWindow: true }, _tab => {
     ReactDOM.render(<Popup />, document.getElementById('popup'));
 });
