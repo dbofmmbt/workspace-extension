ced7afb8b958b2e2948fd6cdf3c35b35b9167a4c
Eduardo Canellas <eduardocanellas98@gmail.com>
2020-12-16 12:32:28 -0300

    feat(popup): add workspace icons to popup

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,21 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Workspace } from "../Workspace";
 import { fetchManager } from "../WorkspaceManager/setup";
+import { Icon } from "./Icon";
 import "./Popup.css";
 
 export default function Popup() {
   const [workspaces, setWorkspaces] = useState<Workspace[]>([]);
+  const [activeWorkspace, setActive] = useState<Workspace | undefined>(
+    undefined
+  );
+
+  const handleClick = useCallback(
+    (workspace: Workspace): void => {
+      if (workspace === activeWorkspace) {
+        return;
+      }
+
+      fetchManager((manager) => {
+        manager.turn_active(workspace);
+        setActive(workspace);
+      });
+    },
+    [activeWorkspace]
+  );
 
   useEffect(() => {
-    fetchManager(manager => {
+    fetchManager((manager) => {
       const workspaces = manager.workspaces();
+      const active = manager.active();
       setWorkspaces(workspaces);
+      setActive(active);
     });
   }, []);
 
-  return (<div className="popupContainer">
-    <ul>
-      {workspaces.map(workspace => <li>{workspace.name}</li>)}
-    </ul>
-  </div>);
+  return (
+    <div className="popupContainer">
+      <div>
+        {workspaces.map((workspace, i) => (
+          <Icon
+            key={i}
+            onClick={() => handleClick(workspace)}
+            isActive={workspace === activeWorkspace}
+            symbol={workspace.symbol}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
